ca14fa4d40fe30d52805aee3b56fc25f09917bec
Dmitry <d.boshchenko@gmail.com>
2026-03-05 19:34:59 +0000

    Заготовка структуры класса Main

--- src/main/java/ru/vk/education/job/Main.java
+++ src/main/java/ru/vk/education/job/Main.java
@@ -1,7 +1,24 @@
 package ru.vk.education.job;
 
+import ru.vk.education.job.cli.CommandProcessor;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandProcessor commandProcessor = new CommandProcessor();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("Input command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("exit")) { break; }
+
+            System.out.println(command);
+            commandProcessor.split(command);
+        }
+
+        scanner.close();
+        System.exit(0);
     }
 }
