3b5cabb346f02fc28cfdaeae45f2ff5fedeb5d42
Dmitry <d.boshchenko@gmail.com>
2026-03-06 14:48:26 +0000

    Разработан класс для хранения пользователей

---
+++ src/main/java/ru/vk/education/job/model/user/UserRepository.java
@@ -0,0 +1,31 @@
+package ru.vk.education.job.model.user;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UserRepository {
+    private static Set<User> users = new HashSet<>();
+    private static Map<String, User> userByName = new HashMap<>();
+
+    public UserRepository() {}
+
+    public UserRepository(User user) {
+        users.add(user);
+        userByName.put(user.getFirstName(), user);
+    }
+
+    public void printList() {
+        System.out.println("List: " + users.size());
+        for (User user : users) {
+            System.out.println(user.getFirstName() + " " +
+                            String.join(",", user.getSkills()) + " " +
+                            user.getExperience());
+        }
+    }
+
+    public boolean find(String firstName) {
+        return userByName.containsKey(firstName);
+    }
+}
