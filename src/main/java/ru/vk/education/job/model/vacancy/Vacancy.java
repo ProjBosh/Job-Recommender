fd3e4fd09de0ae7415aaacee8b02f3b7d80d1168
Dmitry <d.boshchenko@gmail.com>
2026-03-06 15:19:25 +0000

    Разработаны и отредактированы классы Vacancy и VacancyRepository

--- src/main/java/ru/vk/education/job/model/vacancy/Vacancy.java
+++ src/main/java/ru/vk/education/job/model/vacancy/Vacancy.java
@@ -1,17 +1,42 @@
 package ru.vk.education.job.model.vacancy;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Vacancy {
-    private String jobTitle;    // Название вакансии
-    private String company;     // Название компании (дальше можно использовать класс Company)
-    private List<String> tags;  // Теги (дальше можно использовать класс Tag)
-    private int experience;     // Требуемый опыт
+    private String jobTitle;                        // Название вакансии
+    private String company;                         // Название компании (дальше можно использовать класс Company)
+    private Set<String> tags = new HashSet<>();     // Список навыков (аналог skills в классе User)
+    private int experience;                         // Требуемый опыт
 
-    public Vacancy(String jobTitle, String company, List<String> tags, int experience) {
-        this.jobTitle = jobTitle;
-        this.company = company;
-        this.tags = tags;
-        this.experience = experience;
+    public Vacancy() {}
+
+    public Vacancy(String jobTitle, String company, Set<String> tags, int experience) {
+        if (vacancyIsEnteredCorrectly(jobTitle) && !(new VacancyRepository().find(jobTitle))) {
+            this.jobTitle = jobTitle;
+            this.company = company;
+            this.tags = tags;
+            this.experience = experience;
+        }
+    }
+
+    public boolean vacancyIsEnteredCorrectly(String vacancy) {
+        return (vacancy != null && !vacancy.trim().isEmpty() && !vacancy.contains("--"));
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public int getExperience() {
+        return experience;
     }
 }
---
+++ src/main/java/ru/vk/education/job/model/vacancy/VacancyRepository.java
@@ -0,0 +1,25 @@
+package ru.vk.education.job.model.vacancy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class VacancyRepository {
+    private static final Set<Vacancy> vacancies = new HashSet<>();
+    private static final Map<String, Vacancy> vacancyByName = new HashMap<>();
+
+    public VacancyRepository() {}
+
+    public VacancyRepository(Vacancy vacancy) {
+        vacancies.add(vacancy);
+        vacancyByName.put(vacancy.getJobTitle(), vacancy);
+    }
+
+    public void printList() {
+        vacancies.forEach(vacancy -> System.out.println(vacancy.getJobTitle() + " at " +
+                    vacancy.getCompany()));
+    }
+
+    public boolean find(String vacancy) { return vacancyByName.containsKey(vacancy); }
+}
