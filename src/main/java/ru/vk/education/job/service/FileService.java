94c56b7b52b2696857f8671343ae217ddba3629d
Dmitry <d.boshchenko@gmail.com>
2026-04-04 19:26:20 +0300

    Добавлены комментарии и приписка final к неизменяемым параметрам

--- src/main/java/ru/vk/education/job/service/FileService.java
+++ src/main/java/ru/vk/education/job/service/FileService.java
@@ -14,6 +14,9 @@ import java.util.List;
 public class FileService {
     private final Path path;
 
+    /**
+     * Проверяет существование пути к файлу. Добавляет при отстутствии
+     */
     public FileService() {
         String FILE_WITH_COMMAND = "File with command.txt";
         this.path = Paths.get(FILE_WITH_COMMAND);
@@ -26,7 +29,12 @@ public class FileService {
         }
     }
 
-    public void saveCommand(String command) {
+    /**
+     * Сохраняет корректную и выполненную команду в файл
+     *
+     * @param command - Входная команда
+     */
+    public void saveCommand(final String command) {
         try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(path, StandardOpenOption.APPEND), StandardCharsets.UTF_8))) {
             bw.write(command);
             bw.newLine();
@@ -35,6 +43,9 @@ public class FileService {
         }
     }
 
+    /**
+     * Выполняет команды "user ..." и "job ..." при чтение из файла
+     */
     public void executeCommandWithFile() {
         List<String> commandsWithFile = getCommand();
 
@@ -48,6 +59,9 @@ public class FileService {
                 cmdParser.execute(cmd, false);
     }
 
+    /**
+     * Печатает все команды, которые находятся в файле
+     */
     public void printlnCommandWithFile() {
         List<String> commandsWithFile = getCommand();
 
@@ -58,17 +72,18 @@ public class FileService {
             System.out.println(cmd);
     }
 
+    /**
+     * Возвращает пользователю команду из файла
+     * @return Команда из файла
+     */
     private List<String> getCommand() {
         List<String> commands = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(path), StandardCharsets.UTF_8))) {
-            String line = "";
+            String line;
 
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null)
                 commands.add(line.trim());
-                if (line == null)
-                    return new ArrayList<String>();
-            }
         } catch (Exception e) {
             // Игнорируем
         }
