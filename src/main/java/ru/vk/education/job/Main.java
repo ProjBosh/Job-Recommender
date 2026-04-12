f4530bdd27e32af63a147802b3876707ee8798d0
Dmitry <d.boshchenko@gmail.com>
2026-04-12 13:11:01 +0300

    Добавлен вызов Планировщика в код (запуск и выключение)

--- src/main/java/ru/vk/education/job/Main.java
+++ src/main/java/ru/vk/education/job/Main.java
@@ -1,20 +1,23 @@
 package ru.vk.education.job;
 
 import ru.vk.education.job.cli.CommandParser;
-import ru.vk.education.job.model.repository.RecommendationRepository;
 import ru.vk.education.job.service.FileService;
 
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
         boolean getOut = false;
+        int periodDelay = 60; //60
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(3);
         Scanner scanner = new Scanner(System.in);
 
         // Инициализируем чтение команд из файла и испольнение команд "user ..." и "job ..."
         new FileService().executeCommandWithFile();
-//        // Инициализируем заполнение рейтинга совпадений у пользователей по вакансиям
-//        new RecommendationRepository().init();
+
+        // Фоновый подбор лучшей вакансии для всех пользователей
+        StartTheBackgroundProcess(executorService, periodDelay);
 
         while(!getOut) {
             String command = scanner.nextLine().trim();
@@ -26,7 +29,41 @@ public class Main {
             }
         }
 
+        // При завершение программы
+        EndTheBackgroundProcess(executorService, periodDelay);
         scanner.close();
         System.exit(0);
+    }
+
+    /**
+     * Запуск фонового процесса
+     * @param executorService - Сервис исполнитель (Планировщик)
+     * @param periodDelay - период ожидания до следующего запуска
+     */
+    public static void StartTheBackgroundProcess(ScheduledExecutorService executorService, int periodDelay) {
+        executorService.scheduleAtFixedRate(
+                new BestJobSuggestionService(),
+                0,
+                periodDelay,
+                TimeUnit.SECONDS
+        );
+    }
+
+    /**
+     * Завершение фонового процесса
+     * @param executorService - Сервис исполнитель (Планировщик)
+     * @param periodDelay - период ожидания до завершения последней принятой задачи
+     */
+    public static void EndTheBackgroundProcess(ScheduledExecutorService executorService, int periodDelay) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->{
+            executorService.shutdown(); // Прекращаем принимать новые задачи
+            try {
+                // Ожидаем завершения текущих задач
+                if (!executorService.awaitTermination(periodDelay, TimeUnit.SECONDS))
+                    executorService.shutdownNow(); // Принудительно прерываем
+            } catch (InterruptedException e) {
+                executorService.shutdown();
+            }
+        }));
     }
 }
