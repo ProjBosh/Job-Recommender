d3045ff9e47feb0c53612df74f85d1b971c106ec
Dmitry <d.boshchenko@gmail.com>
2026-04-13 20:31:14 +0300

    Отредактирован метод printlnBestJob для получения "свежей" информации и добавлен метод findBestVacancyForUser для получения "свежих данных"

--- src/main/java/ru/vk/education/job/service/BestJobSuggestionService.java
+++ src/main/java/ru/vk/education/job/service/BestJobSuggestionService.java
@@ -2,17 +2,14 @@ package ru.vk.education.job.service;
 
 import ru.vk.education.job.model.model.User;
 import ru.vk.education.job.model.model.Vacancy;
+import ru.vk.education.job.model.rating.RatingVacancy;
+import ru.vk.education.job.model.repository.UserRepository;
 import ru.vk.education.job.model.repository.VacancyRepository;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BestJobSuggestionService implements Runnable {
-    private static Map<User, Vacancy> map = new HashMap<>();
-
     public BestJobSuggestionService() {}
 
     /**
@@ -21,38 +18,44 @@ public class BestJobSuggestionService implements Runnable {
      */
     @Override
     public void run() {
-        // Сортируем по алфавиту нашу карту
-        map = map.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey(Comparator.comparing(User::getFirstName)))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1, e2) -> e1, LinkedHashMap::new
-                ));
-
-        // Выводим лучшие вакансии для каждого пользователя
+        // Выводим лучшее предложение для каждого пользователя
         printlnBestJob();
-    }
-
-    /**
-     * Сохраняет в карту пользователя и его наилучшую ваакансию на данный момент
-     *
-     * @param user - Пользователь
-     * @param vacancy - Вакансия
-     */
-    public void saveRatingVacancyForUser(final User user, final Vacancy vacancy){
-        if (map.containsKey(user))
-            map.replace(user, vacancy);
-        else
-            map.put(user, vacancy);
     }
 
     /**
      * Обходит всех пользователей и выводит лучшую вакансию для пользователя на данный момент
      */
     public void printlnBestJob() {
-        if (!map.isEmpty())
-            map.forEach((u, v) -> System.out.println(u.getFirstName() + ", лучшее предложение — " +
-                    new VacancyRepository().getAJobWithdrawalTemplate(v)));
+        List<Vacancy> vacancies = new VacancyRepository().getVacancies();
+
+        // Выводим лучшие вакансии для каждого пользователя
+        new UserRepository().getUsers().stream()
+                .map(u -> {
+                    Vacancy bestVacancy = findBestVacancyForUser(u, vacancies);
+                    return bestVacancy != null
+                            ? u.getFirstName() + ", лучшее предложение — " + new VacancyRepository().getAJobWithdrawalTemplate(bestVacancy)
+                            : null;
+                })
+                .filter(Objects::nonNull)
+                .forEach(System.out::println);
+    }
+
+    public Vacancy findBestVacancyForUser(User u, List<Vacancy> vacancies) {
+        Vacancy bestVacancy = new Vacancy();
+        double maxPoints = 0;
+        for (Vacancy v : vacancies) {
+            double point = v.countCommonSkills(u.getSkills());
+
+            if (point > 0) {
+                double points = u.getExperience() >= v.getExperience() ? point : (point / 2);
+
+                if (maxPoints < points) {
+                    bestVacancy = v;
+                    maxPoints = points;
+                }
+            }
+        }
+
+        return bestVacancy;
     }
 }
