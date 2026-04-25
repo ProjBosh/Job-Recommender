a120278d0f5a8c3e523d8aea1abab2481ffafe0e
Dmitry <d.boshchenko@gmail.com>
2026-04-25 15:43:15 +0300

    Добавлен стартовый класс SpringApplication

--- src/main/java/ru/vk/education/job/boot/TestApp.java
+++ src/main/java/ru/vk/education/job/boot/TestApp.java
@@ -1,6 +1,11 @@
 package ru.vk.education.job.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TestApp {
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(TestApp.class, args);
+    }
 }
