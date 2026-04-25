bc6459f7cde244c564a9bc95cc9477f8a1ed67bc
Dmitry <d.boshchenko@gmail.com>
2026-04-25 16:37:11 +0300

    Полностью сформирован класс сущности User

--- src/main/java/ru/vk/education/job/boot/domain/User.java
+++ src/main/java/ru/vk/education/job/boot/domain/User.java
@@ -1,4 +1,18 @@
 package ru.vk.education.job.boot.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private String firstname;
+    private Set<String> skills = new HashSet<>();
+    private int experience;
 }
