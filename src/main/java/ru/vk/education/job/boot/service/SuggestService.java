62a742de789d42a27c39662f5667c3502f379c85
Dmitry <d.boshchenko@gmail.com>
2026-04-26 19:33:55 +0300

    Доработан метод обработки команды suggest

--- src/main/java/ru/vk/education/job/boot/service/SuggestService.java
+++ src/main/java/ru/vk/education/job/boot/service/SuggestService.java
@@ -1,4 +1,38 @@
 package ru.vk.education.job.boot.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.vk.education.job.boot.domain.User;
+import ru.vk.education.job.boot.domain.Vacancy;
+import ru.vk.education.job.boot.repository.VacancyRepository;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class SuggestService {
+    private final VacancyRepository vacancyRepository;
+
+    public List<Vacancy> getTopSuggestVacancy(User user, int countVacancy) {
+        Map<Vacancy, Double> ratingSuggestVacancy = new HashMap<>();
+
+        for(Vacancy vacancy : vacancyRepository.findAll()) {
+            int matchCountSkills = (int) vacancyRepository.getTheNumberOfMatchingSkills(vacancy, user);
+            double points = matchCountSkills > 0 && user.getExperience() >= vacancy.getExperience()
+                            ? matchCountSkills
+                            : (double) matchCountSkills / 2;
+            if(points > 0)
+                ratingSuggestVacancy.put(vacancy, points);
+        }
+
+        return ratingSuggestVacancy.entrySet().stream()
+                .sorted(Map.Entry.<Vacancy, Double>comparingByValue(Comparator.reverseOrder())
+                        .thenComparing(entry -> entry.getKey().getJobName()))
+                .limit(countVacancy)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
 }
--- src/main/java/ru/vk/education/job/boot/web/SuggestController.java
+++ src/main/java/ru/vk/education/job/boot/web/SuggestController.java
@@ -1,4 +1,24 @@
 package ru.vk.education.job.boot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.vk.education.job.boot.domain.User;
+import ru.vk.education.job.boot.domain.Vacancy;
+import ru.vk.education.job.boot.service.SuggestService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/suggest")
+@RequiredArgsConstructor
 public class SuggestController {
+    private final SuggestService suggestService;
+
+    @GetMapping
+    public ResponseEntity<List<Vacancy>> getSuggestVacancy(User user) {
+        return ResponseEntity.ok(suggestService.getTopSuggestVacancy(user, 2));
+    }
 }
