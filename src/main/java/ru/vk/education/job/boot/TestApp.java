fc1cb2768877bd835740ffd7f75c91a1eb0622db
Dmitry <d.boshchenko@gmail.com>
2026-04-26 13:44:47 +0300

    Дополнен необходимыми компонентами основной класс приложения

--- src/main/java/ru/vk/education/job/boot/TestApp.java
+++ src/main/java/ru/vk/education/job/boot/TestApp.java
@@ -2,8 +2,10 @@ package ru.vk.education.job.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TestApp {
     public static void main(String[] args) {
         SpringApplication.run(TestApp.class, args);
