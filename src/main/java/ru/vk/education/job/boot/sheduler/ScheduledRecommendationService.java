bf13ace9470f5150e7838c1bba03e9eccc3c60f5
Dmitry <d.boshchenko@gmail.com>
2026-04-26 15:30:07 +0300

    Создан класс для управления потоковыми задачами

---
+++ src/main/java/ru/vk/education/job/boot/sheduler/ScheduledRecommendationService.java
@@ -0,0 +1,9 @@
+package ru.vk.education.job.boot.sheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduledRecommendationService {
+}
