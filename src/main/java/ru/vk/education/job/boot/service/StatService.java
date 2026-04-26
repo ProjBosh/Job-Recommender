380720db82d72f6015cdd234857a683452f299e5
Dmitry <d.boshchenko@gmail.com>
2026-04-26 17:38:13 +0300

    Добавлена обработка команд stat match и комментарии к методам

--- src/main/java/ru/vk/education/job/boot/service/StatService.java
+++ src/main/java/ru/vk/education/job/boot/service/StatService.java
@@ -1,14 +1,13 @@
 package ru.vk.education.job.boot.service;
 
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import ru.vk.education.job.boot.domain.User;
 import ru.vk.education.job.boot.domain.Vacancy;
 import ru.vk.education.job.boot.repository.UserRepository;
 import ru.vk.education.job.boot.repository.VacancyRepository;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -16,9 +15,46 @@ public class StatService {
     private final UserRepository userRepository;
     private final VacancyRepository vacancyRepository;
 
-    public List<Vacancy> getMatchesVacancy(@NotNull User user, @NotNull Integer minExperience) {
+    public List<Vacancy> getMatchesVacancy(int minExperience) {
         return vacancyRepository.findAll().stream()
                 .filter(v -> v.getExperience() >= minExperience)
                 .toList();
+    }
+
+    public List<User> getMatchesUser(int minCountMatch) {
+        // Создаем карту рейтинга по числу подходящих вакансий
+        Map<User, Double> ratingVacancies = new HashMap<>();
+
+        // Для каждого пользователя ищем количество подходящих вакансий
+        for(User user : userRepository.findAll()) {
+            double points = 0;
+            // Получаем количество подходящих вакансий для пользователя по навыкам
+            for(Vacancy vacancy : vacancyRepository.findAll()) {
+                double numberOfMatchingSkills = vacancy.getTags().stream()
+                    .filter(user.getSkills()::contains)
+                    .count();
+                points += numberOfMatchingSkills > 0 ? 1 : 0;
+            }
+            if (points > 0) {
+                // Сохраняем пользователя и количество подходящих ваканий
+                ratingVacancies.put(user, points);
+            }
+        }
+
+        return ratingVacancies.entrySet().stream()
+                .filter(entry -> entry.getValue() >= minCountMatch)
+                .sorted(Comparator.comparing(entry -> entry.getKey().getFirstName()))
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public List<String> getTopSkills(int skillCount) {
+        // TODO: Получить список навыков
+        // 1. Обойти каждого пользователя и получить его навыки
+        // 2. Собрать рейтинг навыков в Map<String, Long>
+        // 3. Получить Топ-N
+        // 4. Вернуть List<String>
+
+        return null;
     }
 }
--- src/main/java/ru/vk/education/job/boot/web/StatController.java
+++ src/main/java/ru/vk/education/job/boot/web/StatController.java
@@ -18,11 +18,42 @@ import java.util.List;
 public class StatController {
     private final StatService statService;
 
+    /**
+     * Поиск и получение вакансий с опытом не меньше переданного числа
+     * @param experience - минимальное количество лет опыта
+     * @return Список подходящих вакансий
+     */
     @GetMapping("/exp")
-    public ResponseEntity<List<Vacancy>> getMatchesVacancy(@NotNull User user, @NotNull Integer experience) {
-        return ResponseEntity.ok(statService.getMatchesVacancy(user, experience));
+    public ResponseEntity<List<Vacancy>> getMatchesVacancy(int experience) {
+        if (experience < 0) {
+            experience = 0;
+        }
+        return ResponseEntity.ok(statService.getMatchesVacancy(experience));
     }
 
-//    @PostMapping
-//    public ResponseEntity<Vacancy>
+    /**
+     * Поиск и получение пользователей с количеством подходящих вакансий не меньше переданного числа
+     * @param matchCount - минимальное количество подходящих вакансий
+     * @return Список подходящих пользователей
+     */
+    @GetMapping("/match")
+    public ResponseEntity<List<User>> getMatchesUser(int matchCount) {
+        if (matchCount < 0) {
+            matchCount = 0;
+        }
+        return ResponseEntity.ok(statService.getMatchesUser(matchCount));
+    }
+
+    /**
+     * Поиск и получение Топ-N указанных пользователями навыков
+     * @param skillCount - количестов получаемых навыков
+     * @return Список Топ-N навыков
+     */
+    @GetMapping("/top-skills")
+    public ResponseEntity<List<String>> getTopSkills(int skillCount) {
+        if (skillCount < 0) {
+            skillCount = 0;
+        }
+        return ResponseEntity.ok(statService.getTopSkills(skillCount));
+    }
 }
