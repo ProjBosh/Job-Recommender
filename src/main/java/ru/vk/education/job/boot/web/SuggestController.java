7d088a4693cc973453473ede9f70f20a327cc44d
Dmitry <d.boshchenko@gmail.com>
2026-04-27 16:35:06 +0300

    Оптимизировано получение значения 2 для получения Топ-2 вакансий для пользователя

--- src/main/java/ru/vk/education/job/boot/web/SuggestController.java
+++ src/main/java/ru/vk/education/job/boot/web/SuggestController.java
@@ -1,6 +1,7 @@
 package ru.vk.education.job.boot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;  // ← правильный импорт
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +21,12 @@ public class SuggestController {
     private final SuggestService suggestService;
     private final UserService userService;
 
+    @Value("${suggest.top.limit:2}")
+    private int topVacancyLimit;
+
     @GetMapping("/{userId}")
     public ResponseEntity<List<Vacancy>> getSuggestVacancy(@PathVariable Long userId) {
         User user = userService.getUser(userId);
-        return ResponseEntity.ok(suggestService.getTopSuggestVacancy(user, 2));
+        return ResponseEntity.ok(suggestService.getTopSuggestVacancy(user, topVacancyLimit));
     }
 }
