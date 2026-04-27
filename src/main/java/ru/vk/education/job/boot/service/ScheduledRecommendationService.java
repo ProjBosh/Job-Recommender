3cc810094f34ce51726a95e838926eaf4d89d65b
Dmitry <d.boshchenko@gmail.com>
2026-04-27 16:38:09 +0300

    Удален класс

--- src/main/java/ru/vk/education/job/boot/service/ScheduledRecommendationService.java
+++
@@ -1,9 +0,0 @@
-package ru.vk.education.job.boot.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class ScheduledRecommendationService {
-}
