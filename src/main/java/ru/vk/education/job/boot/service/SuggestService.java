a161b7908a305d0e7e99a9d2f856ae5c55311942
Dmitry <d.boshchenko@gmail.com>
2026-04-27 16:39:01 +0300

    Реализован метод findBestVacancies для фонового поиска лучшей вакансии

--- src/main/java/ru/vk/education/job/boot/service/SuggestService.java
+++ src/main/java/ru/vk/education/job/boot/service/SuggestService.java
@@ -4,23 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.vk.education.job.boot.domain.User;
 import ru.vk.education.job.boot.domain.Vacancy;
+import ru.vk.education.job.boot.repository.UserRepository;
 import ru.vk.education.job.boot.repository.VacancyRepository;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class SuggestService {
     private final VacancyRepository vacancyRepository;
+    private final UserRepository userRepository;
+    private final VacancyService vacancyService;
 
     public List<Vacancy> getTopSuggestVacancy(User user, int countVacancy) {
         Map<Vacancy, Double> ratingSuggestVacancy = new HashMap<>();
 
         for(Vacancy vacancy : vacancyRepository.findAll()) {
-            int matchCountSkills = (int) vacancyRepository.getTheNumberOfMatchingSkills(vacancy, user);
+            int matchCountSkills = (int) vacancyService.getTheNumberOfMatchingSkills(vacancy, user);
             double points = matchCountSkills > 0 && user.getExperience() >= vacancy.getExperience()
                             ? matchCountSkills
                             : (double) matchCountSkills / 2;
@@ -34,5 +34,13 @@ public class SuggestService {
                 .limit(countVacancy)
                 .map(Map.Entry::getKey)
                 .toList();
+    }
+
+    public void findBestVacancies() {
+        for(User user : userRepository.findAll()) {
+            Vacancy bestVacancy = getTopSuggestVacancy(user, 1).get(0);
+            System.out.println(user.getFirstName() + ", лучшее предложение - " +
+                        bestVacancy.getJobName() + " at " + bestVacancy.getCompany());
+        }
     }
 }
