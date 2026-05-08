e031d1ffa275437c2b40cddf7adb6e98eebedfa7
Dmitry <d.boshchenko@gmail.com>
2026-05-08 11:09:43 +0300

    Изменен тип у id с Long на UUID у класса Vacancy

--- src/main/java/ru/vk/education/job/boot/domain/Vacancy.java
+++ src/main/java/ru/vk/education/job/boot/domain/Vacancy.java
@@ -4,13 +4,14 @@ import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Vacancy {
-    private Long id;
+    private UUID id;
     private String jobName;
     private String company;
     private Set<String> tags = new HashSet<>();
